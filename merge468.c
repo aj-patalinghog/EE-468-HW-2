e59bc3e3ffe0233b6094d1c502fd745c57235554
aj-patalinghog <99686240+aj-patalinghog@users.noreply.github.com>
2023-09-20 09:45:34 -1000

    Formatting

--- merge468.c
+++ merge468.c
@@ -19,7 +19,6 @@
  * so forth.
  *
  * Use the pthread API
- *
  */
 
 #include <stdio.h>
@@ -28,48 +27,46 @@
 
 #define N 32 
 
-/* Global variables -- be careful */
-int a[N];     /* Array to sort */
-int temp[N];  /* Temporary storage */
+// Global variables -- be careful
+int a[N];     // Array to sort
+int temp[N];  // Temporary storage
 
-void genvalues(int n); /* Initializes array a[] */
-void prnvalues(int n); /* Prints array a[] */
-void merge(int first, int midpt, int last); /* Merges subarrays */
+void genvalues(int n); // Initializes array a[]
+void prnvalues(int n); // Prints array a[]
+void merge(int first, int midpt, int last); // Merges subarrays
 
-main() 
-{
+main() {
 
-int arrsize;     /* Size of subarrays to merge */
-int numarr;      /* Number of subarrays */
-int newarrsize;  /* New subarray size */
-int newnumarr;   /* New number of subarrays */
-int i;
-int first;       /* Parameters used to merge two subarrays */
-int midpt;       /*   The first subarray starts at "first" and ends */ 
-int last;        /*   at "first+midpt-1".  The second subarray starts */
-                 /*   at "first+midpt" and ends at "last-1" */
+   int arrsize;     // Size of subarrays to merge
+   int numarr;      // Number of subarrays
+   int newarrsize;  // New subarray size
+   int newnumarr;   // New number of subarrays
+   int i;
+   int first;       // Parameters used to merge two subarrays
+   int midpt;       //   The first subarray starts at "first" and ends 
+   int last;        //   at "first+midpt-1".  The second subarray starts
+                    //   at "first+midpt" and ends at "last-1"
 
-genvalues(N);    /* Initialize a[] with random values */
-printf("Initial values:\n");
-prnvalues(N);    /* Display the values */
+   genvalues(N);    // Initialize a[] with random values
+   printf("Initial values:\n");
+   prnvalues(N);    // Display the values
 
-arrsize = 1;
+   arrsize = 1;
 
-while (arrsize < N) {
-   printf("*** Merging subarrays of size %d\n",arrsize);
-   arrsize= 2*arrsize; /* merge subarrays to double subarray size */
-   for (i=0; i<N; i+=arrsize) {
-      first = i;
-      midpt = first +(arrsize/2);
-      if (first + arrsize < N) last = first + arrsize;
-      else last = N;
-      merge(first, midpt, last);
+   while (arrsize < N) {
+      printf("*** Merging subarrays of size %d\n",arrsize);
+      arrsize= 2*arrsize; // merge subarrays to double subarray size
+      for (i=0; i<N; i+=arrsize) {
+         first = i;
+         midpt = first +(arrsize/2);
+         if (first + arrsize < N) last = first + arrsize;
+         else last = N;
+         merge(first, midpt, last);
+      }
    }
-}
 
-printf("\nOutput:\n");
-prnvalues(N); /* Display the sorted values */
-
+   printf("\nOutput:\n");
+   prnvalues(N); // Display the sorted values
 }
 
 /*
@@ -79,69 +76,59 @@ prnvalues(N); /* Display the sorted values */
  *    Then it copies temp[first],..., temp[last-1] back into
  *    a[first],..., a[last-1].
  */
-void merge(int first, int midpt, int last)
-{
-int leftptr;   /* Pointers used in array a[ ] */
-int rightptr;
-int k;         /* pointer used in array temp[ ] */
-int delay;
 
-/* 
- * Do not delete the next three lines.  They cause the function to
- * delay by a amount that is proportional to the subarray it is merging
- */
-if (last-first > 0) delay = last-first;
-else delay = 1;
-usleep(delay*250000);
+void merge(int first, int midpt, int last) {
+   int leftptr;   // Pointers used in array a[ ]
+   int rightptr;
+   int k;         // pointer used in array temp[ ]
+   int delay;
 
-leftptr = first;
-rightptr = midpt; 
+   /*
+    * Do not delete the next three lines.  They cause the function to
+    * delay by a amount that is proportional to the subarray it is merging
+    */
+   
+   if (last-first > 0) delay = last-first;
+   else delay = 1;
+   usleep(delay*250000);
 
-/* Merge values in the two arrays of a[] into temp[] */
-for(k=first; k<last; k++) {
-   if (leftptr >= midpt) temp[k] = a[rightptr++];
-   else if (rightptr >= last) temp[k] = a[leftptr++];
-   else if (a[leftptr] < a[rightptr]) temp[k] = a[leftptr++];
-   else if (a[leftptr] >= a[rightptr]) temp[k] = a[rightptr++];
-   else printf("There's a bug \n");
+   leftptr = first;
+   rightptr = midpt; 
+
+   // Merge values in the two arrays of a[] into temp[]
+   for(k=first; k<last; k++) {
+      if (leftptr >= midpt) temp[k] = a[rightptr++];
+      else if (rightptr >= last) temp[k] = a[leftptr++];
+      else if (a[leftptr] < a[rightptr]) temp[k] = a[leftptr++];
+      else if (a[leftptr] >= a[rightptr]) temp[k] = a[rightptr++];
+      else printf("There's a bug \n");
+   }
+
+   // Copy temp[] back to a[]
+   for(k=first; k<last; k++) a[k] = temp[k];
 }
 
-/* Copy temp[] back to a[] */
-for(k=first; k<last; k++) a[k] = temp[k];
+// Initializes array a[] with random values.
+void genvalues(int n) {
+   int k;
+   int i;
+   int current; 
 
+   k = 2*n;
+   current = 0;
+   for (i=0; i<n; i++) {
+      current = (current*73 + 19)%k;
+      a[i] = current;
+   }
 }
 
-
-
-/*
- * Initializes array a[] with random values.
- */
-void genvalues(int n)
-{
-int k;
-int i;
-int current; 
-
-k = 2*n;
-current = 0;
-for (i=0; i<n; i++) {
-   current = (current*73 + 19)%k;
-   a[i] = current;
+// Prints the values in the array a[]
+void prnvalues(int n) {
+   int i;
+   for (i=0; i<n; i++) {
+      printf(" %d ", a[i]);
+      if ((i+1)%10 == 0) printf("\n");
+   }
+   printf("\n");
 }
-}
-
-/*
- * Prints the values in the array a[]
- */
-void prnvalues(int n)
-{
-int i;
-for (i=0; i<n; i++) {
-   printf(" %d ", a[i]);
-   if ((i+1)%10 == 0) printf("\n");
-}
-printf("\n");
-
-}
-
 
